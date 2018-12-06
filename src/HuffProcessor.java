9ff917c1b5397c5321de66616525f17f758eb2d1
Caleb Chiang <csc.679@gmail.com>
2018-12-06 10:56:17 -0500

    progress1

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 /**
  * Although this class has a history of several years,
@@ -42,13 +43,72 @@ public class HuffProcessor {
 	 */
 	public void compress(BitInputStream in, BitOutputStream out){
 
-		while (true){
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1) break;
-			out.writeBits(BITS_PER_WORD, val);
-		}
+		int[] counts = readForCounts(in, out);
+		HuffNode root = makeTreeFromCounts(counts);
+		String[] codings = makeCodingsFromTree(root);
+		
+		out.writeBits(BITS_PER_INT, HUFF_TREE);
+		writeHeader(root, out);
+		
+		in.reset();
+		writeCompressedBits(codings, in, out);
 		out.close();
 	}
+	
+	public int[] readForCounts(BitInputStream in, BitOutputStream out) {
+		
+		int[] freq = new int[(ALPH_SIZE + 1)];
+		
+		freq[PSEUDO_EOF] = 1;
+		
+		while(true) {
+			
+			int bits = in.readBits(BITS_PER_WORD);
+			
+			if (bits == -1) break;
+			
+			int ascii = Integer.parseInt(String.valueOf(bits), 2);
+			freq[ascii] += 1;
+			
+			out.writeBits(BITS_PER_WORD, bits);
+		}
+		out.close();
+		return freq;
+	}
+	
+	public HuffNode makeTreeFromCounts(int[] counts) {
+		
+		PriorityQueue<HuffNode> pq = new PriorityQueue<>();
+
+		for(int i = 0; i < counts.length; i ++ ) {
+			if (counts[i] > 0) {
+				pq.add(new HuffNode(i, counts[i], null, null)); 
+			}
+		    
+		}
+
+		while (pq.size() > 1) {
+			
+		    HuffNode left = pq.remove();
+		    HuffNode right = pq.remove();
+		    HuffNode t = new HuffNode(0, left.myWeight + right.myWeight, left, right); 
+		    pq.add(t);
+		}
+		
+		HuffNode root = pq.remove();
+		return root;
+	}
+	
+	public String[] makeCodingsFromTree(HuffNode root) {
+		String[] encodings = new String[ALPH_SIZE + 1];
+	    codingHelper(root,"",encodings);
+
+	}
+	
+	public void codingHelper(HuffNode root, String word, String[] encodings) {
+		
+	}
+	
 	/**
 	 * Decompresses a file. Output file must be identical bit-by-bit to the
 	 * original.
@@ -60,11 +120,72 @@ public class HuffProcessor {
 	 */
 	public void decompress(BitInputStream in, BitOutputStream out){
 
-		while (true){
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1) break;
-			out.writeBits(BITS_PER_WORD, val);
+		int magicbits = in.readBits(BITS_PER_INT);
+		if (magicbits != HUFF_TREE) {
+			throw new HuffException("illegal header starts with " + magicbits);
+			
 		}
+		
+		HuffNode root = readTreeHeader(in);
+		readCompressedBits(root, in, out);
 		out.close();
 	}
+	
+	public HuffNode readTreeHeader(BitInputStream in) {
+			
+			int bits = in.readBits(1);
+			HuffNode huff;
+			
+			if (bits == -1) throw new HuffException("reading bits has failed");
+			
+			if (bits == 0) {
+			    HuffNode left = readTreeHeader(in);
+			    HuffNode right = readTreeHeader(in);
+			    return new HuffNode(0, 0, left, right); 
+			}
+			
+			else {
+			    int morebits = in.readBits(BITS_PER_INT + 1);
+			    return new HuffNode(morebits, 0, null, null);
+			}
+	}
+	
+	public void readCompressedBits(HuffNode root, BitInputStream in, BitOutputStream out) {
+			
+			HuffNode current = root; 
+			
+				while (true) {
+					
+					int bits = in.readBits(1);
+					if (bits == -1) throw new HuffException("bad input, no PSEUDO_EOF");
+					
+					else { 
+						
+						if (bits == 0) current = current.myLeft;
+						
+						else current = current.myRight;	 
+							
+						// if current is a leaf node
+						if (current.myLeft == null && current.myRight == null) {
+							
+							if (current.myValue == PSEUDO_EOF) {
+								
+								break;   // out of loop
+								
+							}
+			                   
+				            else {
+				            	
+				            	out.writeBits(BITS_PER_WORD, current.myValue);
+				            	current = root; 
+				            	
+				            }
+						}
+					}
+				}
+				
+			in.close();
+			out.close(); 
+	}
+	
 }
